86e97b8f8fa5212f44073a23475cd8b8acc739e6
patilmalhar108 <patilmalhar.a@gmail.com>
2026-05-07 06:48:53 -0500

    Add files via upload

---
+++ advancevisulization.py
@@ -0,0 +1,44 @@
+print("Advance Visuilization on Weather")
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set(style = 'ticks')
+
+weather = pd.read_csv("test.csv")
+print(weather.head())
+
+print(weather.info())
+
+sns.barplot(x = weather['humidity'], y = weather['temperature'])
+plt.show()
+
+sns.distplot(weather['humidity'], kde = False, rug = True)
+plt.show()
+
+sns.jointplot(x = weather['humidity'], y = weather['temperature'], kind = "hex")
+plt.show()
+
+sns.jointplot(x = weather['humidity'], y = weather['temperature'], kind = "kde")
+plt.show()
+
+sns.pairplot(weather[['humidity', 'temperature', 'air_pollution_index']])
+plt.show()
+
+sns.stripplot(x = weather['weather_type'], y = weather['temperature'])
+plt.show()
+
+sns.stripplot(x = weather['weather_type'], y = weather['temperature'], jitter = True)
+plt.show()
+
+sns.swarmplot(x = weather['weather_type'], y = weather['temperature'])
+plt.show()
+
+sns.barplot(x = weather['humidity'], y = weather['temperature'], hue = weather['weather_type'])
+plt.show()
+
+sns.countplot(x = weather['weather_type'])
+plt.show()
+
+sns.pointplot(x = weather['humidity'], y = weather['temperature'], hue = weather['weather_type'])
+plt.show()
